23d94d1a4bc3325070b44f46a05e8ef44067f4ab
Shailesh Gharti <124006792+Men-ace@users.noreply.github.com>
2025-04-22 13:22:46 +1000

    cloudianry notification post route

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/user.route.js"
 import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser"
+import postRoutes from "./routes/post.route.js"
  
 dotenv.config()
 const app = express()
@@ -12,6 +14,8 @@ app.use(cookieParser)
 const PORT = process.env.PORT ||  5000
 
 app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/users", userRoutes)
+app.use("/api/v1/posts", postRoutes)
 
 
 app.listen(PORT, () => {
