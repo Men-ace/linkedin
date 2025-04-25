7c2f4c5b68634d0d119011b35ab9f2519bb650fc
Shailesh Gharti <124006792+Men-ace@users.noreply.github.com>
2025-04-25 20:16:32 +1000

    testting new loaction

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use("/api/v1/posts", postRoutes)
 app.use("/api/v1/notifications", notificationRoutes)
 app.use("/api/v1/connections", connectionRoute)
 
-
+//
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
     connectDB()
